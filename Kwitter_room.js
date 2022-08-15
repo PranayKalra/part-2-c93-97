c9335e091bd2da6bd4ff89a6bd3a5b818ce3b516
PranayKalra <90055057+PranayKalra@users.noreply.github.com>
2022-08-15 14:40:09 -0400

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,16 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDgsePKC9Kj-2wamyPbv3esst2XeArkTyM",
+    authDomain: "c93-97-22cfd.firebaseapp.com",
+    databaseURL: "https://c93-97-22cfd-default-rtdb.firebaseio.com",
+    projectId: "c93-97-22cfd",
+    storageBucket: "c93-97-22cfd.appspot.com",
+    messagingSenderId: "193723579181",
+    appId: "1:193723579181:web:2f6a4c2d1ebee5b530f1a4"
+  };
+  
+  
+  firebase.initializeApp(firebaseConfig);
+
+
+function Logout(){
+}
